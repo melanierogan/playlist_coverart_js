22317f411a26d5b02df1886a36595707e27bcc3f
Melanie Rogan <melanie.rogan@ft.com>
2019-05-26 20:16:09 +0100

    express up and running

--- server/app.js
+++ server/app.js
@@ -1,8 +1,30 @@
 const express = require('express')
-const app = express()
+const handlebars = require('express-handlebars');
+
+const app = new express();
+
+const handlebarsInstance = handlebars.create({ 
+	extname: '.html',
+	defaultLayout: 'main'
+
+});
+app.engine('html', handlebarsInstance.engine);
+app.set('view engine', '.html');
+app.use(express.static('public'));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+app.get('/', (req, res) => {
+	res.render('layouts/home', {
+		title: 'Home',
+		pageTitle: 'Hello 👋' 
+	});
+});
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+// app.listen(3000)
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('\n App listening on port:', port, '\n');
+});
